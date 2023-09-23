66f24ebdcf30353f583246c848b387626d8a630f
Radek <gosciminskiradoslaw@gmail.com>
2023-09-23 10:23:21 +0200

    Fetching orders

---
+++ src/features/order/SearchOrder.tsx
@@ -0,0 +1,25 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
